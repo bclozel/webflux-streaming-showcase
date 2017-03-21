ce095b8dadb959a9ea9ff08de5ec3fc42ae01483
Brian Clozel <bclozel@pivotal.io>
2017-03-21 14:32:57 +0100

    Add Mustache views support
    to be removed once Spring Boot issue 8648 is resolved

--- streaming-service/src/main/java/io/spring/demo/streaming/StreamingServiceApplication.java
+++ streaming-service/src/main/java/io/spring/demo/streaming/StreamingServiceApplication.java
@@ -1,7 +1,14 @@
 package io.spring.demo.streaming;
 
+
+import com.samskivert.mustache.Mustache;
+import io.spring.demo.streaming.support.MustacheResourceTemplateLoader;
+import io.spring.demo.streaming.support.MustacheViewResolver;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StreamingServiceApplication {
@@ -10,5 +17,15 @@ public class StreamingServiceApplication {
 		SpringApplication.run(StreamingServiceApplication.class, args);
 	}
 
-	
+	@Bean
+	public MustacheViewResolver mustacheViewResolver(MustacheProperties properties) {
+		MustacheViewResolver viewResolver = new MustacheViewResolver();
+		viewResolver.setPrefix(properties.getPrefix());
+		viewResolver.setSuffix(properties.getSuffix());
+		MustacheResourceTemplateLoader loader =
+				new MustacheResourceTemplateLoader(properties.getPrefix(), properties.getSuffix());
+		viewResolver.setCompiler(Mustache.compiler().escapeHTML(false).withLoader(loader));
+		return viewResolver;
+	}
+
 }
