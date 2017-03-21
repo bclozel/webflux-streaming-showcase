61ef47d4311a48946b87188d3ee6146ea4390c29
Brian Clozel <bclozel@pivotal.io>
2017-03-21 14:29:54 +0100

    Create streaming-service application skeleton

---
+++ streaming-service/src/main/java/io/spring/demo/streaming/StreamingServiceApplication.java
@@ -0,0 +1,14 @@
+package io.spring.demo.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingServiceApplication.class, args);
+	}
+
+	
+}
