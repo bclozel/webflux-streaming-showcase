f74921632974e0136216c2200d2d256085cfd2ed
Brian Clozel <bclozel@pivotal.io>
2017-03-22 15:03:39 +0100

    Create UserRepository with reactive mongodb

--- streaming-service/src/main/java/io/spring/demo/streaming/StreamingServiceApplication.java
+++ streaming-service/src/main/java/io/spring/demo/streaming/StreamingServiceApplication.java
@@ -1,7 +1,16 @@
 package io.spring.demo.streaming;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import io.spring.demo.streaming.portfolio.User;
+import io.spring.demo.streaming.portfolio.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StreamingServiceApplication {
@@ -10,5 +19,19 @@ public class StreamingServiceApplication {
 		SpringApplication.run(StreamingServiceApplication.class, args);
 	}
 
-	
+	@Bean
+	public CommandLineRunner createUsers(UserRepository userRepository) {
+		return strings -> {
+			List<User> users = Arrays.asList(
+					new User("sdeleuze", "Sebastien Deleuze"),
+					new User("snicoll", "Stephane Nicoll"),
+					new User("rstoyanchev", "Rossen Stoyanchev"),
+					new User("smaldini", "Stephane Maldini"),
+					new User("sbasle", "Simon Basle"),
+					new User("bclozel", "Brian Clozel")
+			);
+			userRepository.save(users).blockLast(Duration.ofSeconds(3));
+		};
+	}
+
 }
