ade6f51aa6a4b5e205d7e430a47f747cdd54bdef
Olivier Truong <olivier@nadeva.fr>
2019-02-18 11:09:03 +0100

    migrate

--- quote-stream/src/main/java/io/spring/demo/quotes/QuoteRouter.java
+++ quote-stream/src/main/java/io/spring/demo/quotes/QuoteRouter.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.Rendering;
 
 import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
@@ -17,7 +18,8 @@ public class QuoteRouter {
 	@Bean
 	public RouterFunction<ServerResponse> route(QuoteHandler quoteHandler) {
 		return RouterFunctions
-				.route(GET("/quotes").and(accept(TEXT_EVENT_STREAM)), quoteHandler::fetchQuotesSSE)
+				.route(GET("/").and(accept(TEXT_EVENT_STREAM)), quoteHandler::fetchQuotesSSE)
+				.andRoute(GET("/quotes").and(accept(TEXT_EVENT_STREAM)), quoteHandler::fetchQuotesSSE)
 				.andRoute(GET("/quotes").and(accept(APPLICATION_STREAM_JSON)), quoteHandler::fetchQuotes);
 	}
 }
